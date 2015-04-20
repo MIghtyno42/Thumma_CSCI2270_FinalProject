326564edddaef3fb5b0325f731e142ae3dc25ac2
DThumma <deekshitha.thumma@colorado.edu>
2015-04-20 10:30:35 -0600

    Added a main driver with main skeleton

---
+++ main.cpp
@@ -0,0 +1,20 @@
+// Deekshitha Thumma
+// Hoenigman 2270
+// FInal Project
+// April 20th
+// Partner: Christine Samson
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <vector>
+#include "Fashion.h"
+
+using namespace std;
+
+int main()
+{
+
+}
+
