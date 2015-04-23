65b9bf088f711fbae97ea233c78faef6263ef138
DThumma <deekshitha.thumma@colorado.edu>
2015-04-23 14:09:05 -0600

    Made Print INventory print all

--- Fashion.cpp
+++ Fashion.cpp
@@ -27,5 +27,8 @@ void Fashion::printInventory(item *foundItems, int sizeStock)
 {
 	for(int i = 0; i < sizeStock; i++){
 		cout<<"Name: "<<foundItems[i].name<<endl;
+		cout<<"Type:" <<foundItems[i].type<<endl;
+		cout<<"Color:" <<foundItems[i].color<<endl;
+		cout<<"Style:" <<foundItems[i].style<<endl;
 	}
 }
